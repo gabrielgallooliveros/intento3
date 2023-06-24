a7fd1bea7c8a83e2a79788ec33269d538a95a3ea
gabrielgallooliveros@gmail.com <gabrielgallooliveros@gmail.com>
2023-06-24 16:15:11 -0500

    completo

--- app.js
+++ app.js
@@ -60,10 +60,10 @@ function efectoHabilidades(){
             pintarBarra(html, 16, 0, intervalHtml);
         },100);
         const intervalJavascript = setInterval(function(){
-            pintarBarra(javascript, 11, 1, intervalJavascript);
+            pintarBarra(javascript, 15, 1, intervalJavascript);
         },100);
         const intervalWordpress = setInterval(function(){
-            pintarBarra(wordpress, 11, 2, intervalWordpress);
+            pintarBarra(wordpress, 16, 2, intervalWordpress);
         },100);
         const intervalPhotoshop = setInterval(function(){
             pintarBarra(photoshop, 15, 3, intervalPhotoshop);
@@ -72,7 +72,7 @@ function efectoHabilidades(){
             pintarBarra(php, 16, 4, intervalPhp);
         },100);
         const intervalIlustrator = setInterval(function(){
-            pintarBarra(ilustrator, 11, 5, intervalIlustrator);
+            pintarBarra(ilustrator, 16, 5, intervalIlustrator);
         },100);
     }
 }
